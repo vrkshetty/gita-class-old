e500c7c948b0b3b143a9b6445f953651dafb85b9
vrkshetty <vijayrkshetty@gmail.com>
2021-05-27 17:25:44 +0100

    added shlokas

--- data.js
+++ data.js
@@ -59,4 +59,156 @@ var myList = [
     Text: "<p>karmaṇy-evādhikāras te mā phaleṣhu kadāchana<br>mā karma-phala-hetur bhūr mā te saṅgo ’stvakarmaṇi</p>",
     audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_047.mp3",
   },
+  {
+    Tittle: "Chapter-2 , Verse-63",
+    Text: "<p>krodhād bhavati sammohaḥ sammohāt smṛiti-vibhramaḥ<br>smṛiti-bhranśhād buddhi-nāśho buddhi-nāśhāt praṇaśhyati</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_063.mp3",
+  },
+  {
+    Tittle: "Chapter-2 , Verse-70",
+    Text: "<p>āpūryamāṇam achala-pratiṣhṭhaṁ<br>samudram āpaḥ praviśhanti yadvat<br>tadvat kāmā yaṁ praviśhanti sarve<br>sa śhāntim āpnoti na kāma-kāmī</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_070.mp3",
+  },
+  {
+    Tittle: "Chapter-3 , Verse-13",
+    Text: "<p>yajña-śhiṣhṭāśhinaḥ santo muchyante sarva-kilbiṣhaiḥ<br>bhuñjate te tvaghaṁ pāpā ye pachantyātma-kāraṇāt</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/003_013.mp3",
+  },
+  {
+    Tittle: "Chapter-3 , Verse-27",
+    Text: "<p>prakṛiteḥ kriyamāṇāni guṇaiḥ karmāṇi sarvaśhaḥ<br>ahankāra-vimūḍhātmā kartāham iti manyate</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/003_027.mp3",
+  },
+  {
+    Tittle: "Chapter-4 , Verse-2",
+    Text: "<p>evaṁ paramparā-prāptam imaṁ rājarṣhayo viduḥ<br>sa kāleneha mahatā yogo naṣhṭaḥ parantapa</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/004_002.mp3",
+  },
+  {
+    Tittle: "Chapter-4 , Verse-7",
+    Text: "<p>yadā yadā hi dharmasya glānir bhavati bhārata<br>abhyutthānam adharmasya tadātmānaṁ sṛijāmyaham</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/004_007.mp3",
+  },
+  {
+    Tittle: "Chapter-4 , Verse-8",
+    Text: "<p>paritrāṇāya sādhūnāṁ vināśhāya cha duṣhkṛitām<br>dharma-sansthāpanārthāya sambhavāmi yuge yuge</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/004_008.mp3",
+  },
+  {
+    Tittle: "Chapter-4 , Verse-9",
+    Text: "<p>janma karma cha me divyam evaṁ yo vetti tattvataḥ<br>tyaktvā dehaṁ punar janma naiti mām eti so ’rjuna</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/004_009.mp3",
+  },
+  {
+    Tittle: "Chapter-4 , Verse-11",
+    Text: "<p>ye yathā māṁ prapadyante tāns tathaiva bhajāmyaham<br>mama vartmānuvartante manuṣhyāḥ pārtha sarvaśhaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/004_011.mp3",
+  },
+  {
+    Tittle: "Chapter-4 , Verse-34",
+    Text: "<p>tad viddhi praṇipātena paripraśhnena sevayā<br>upadekṣhyanti te jñānaṁ jñāninas tattva-darśhinaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/004_034.mp3",
+  },
+  {
+    Tittle: "Chapter-5 , Verse-18",
+    Text: "<p>vidyā-vinaya-sampanne brāhmaṇe gavi hastini<br>śhuni chaiva śhva-pāke cha paṇḍitāḥ sama-darśhinaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/005_018.mp3",
+  },
+  {
+    Tittle: "Chapter-5 , Verse-22",
+    Text: "<p>ye hi sansparśha-jā bhogā duḥkha-yonaya eva te<br>ādyantavantaḥ kaunteya na teṣhu ramate budhaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/005_022.mp3",
+  },
+  {
+    Tittle: "Chapter-5 , Verse-29",
+    Text: "<p>bhoktāraṁ yajña-tapasāṁ sarva-loka-maheśhvaram<br>suhṛidaṁ sarva-bhūtānāṁ jñātvā māṁ śhāntim ṛichchhati</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/005_029.mp3",
+  },
+  {
+    Tittle: "Chapter-6 , Verse-5",
+    Text: "<p>uddhared ātmanātmānaṁ nātmānam avasādayet<br>ātmaiva hyātmano bandhur ātmaiva ripur ātmanaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/006_005.mp3",
+  },
+  {
+    Tittle: "Chapter-7 , Verse-3",
+    Text: "<p>manuṣhyāṇāṁ sahasreṣhu kaśhchid yatati siddhaye<br>yatatām api siddhānāṁ kaśhchin māṁ vetti tattvataḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/007_005.mp3",
+  },
+
+  {
+    Tittle: "Chapter-8 , Verse-5",
+    Text: "<p>anta-kāle cha mām eva smaran muktvā kalevaram<br>yaḥ prayāti sa mad-bhāvaṁ yāti nāstyatra sanśhayaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/008_005.mp3",
+  },
+  {
+    Tittle: "Chapter-8 , Verse-6",
+    Text: "<p>yaṁ yaṁ vāpi smaran bhāvaṁ tyajatyante kalevaram<br>taṁ tam evaiti kaunteya sadā tad-bhāva-bhāvitaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/008_006.mp3",
+  },
+
+  {
+    Tittle: "Chapter-10 , Verse-11",
+    Text: "<p>teṣhām evānukampārtham aham ajñāna-jaṁ tamaḥ<br>nāśhayāmyātma-bhāva-stho jñāna-dīpena bhāsvatā</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/010_011.mp3",
+  },
+  {
+    Tittle: "Chapter-10 , Verse-12-13",
+    Text: "<p>arjuna uvācha<br> <br>paraṁ brahma paraṁ dhāma pavitraṁ paramaṁ bhavān<br>puruṣhaṁ śhāśhvataṁ divyam ādi-devam ajaṁ vibhum<br>āhus tvām ṛiṣhayaḥ sarve devarṣhir nāradas tathā<br>asito devalo vyāsaḥ svayaṁ chaiva bravīṣhi me</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/010_012-013.mp3",
+  },
+  {
+    Tittle: "Chapter-10 , Verse-41",
+    Text: "<p>yad yad vibhūtimat sattvaṁ śhrīmad ūrjitam eva vā<br>tat tad evāvagachchha tvaṁ mama tejo ’nśha-sambhavam</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/010_041.mp3",
+  },
+  {
+    Tittle: "Chapter-11 , Verse-54",
+    Text: "<p>bhaktyā tv ananyayā śhakya aham evaṁ-vidho ’rjuna<br>jñātuṁ draṣhṭuṁ cha tattvena praveṣhṭuṁ cha parantapa</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/011_054.mp3",
+  },
+  {
+    Tittle: "Chapter-11 , Verse-55",
+    Text: "<p>mat-karma-kṛin mat-paramo mad-bhaktaḥ saṅga-varjitaḥ<br>nirvairaḥ sarva-bhūteṣhu yaḥ sa mām eti pāṇḍava</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/011_055.mp3",
+  },
+  {
+    Tittle: "Chapter-12 , Verse-5",
+    Text: "<p>kleśho ’dhikataras teṣhām avyaktāsakta-chetasām<br>avyaktā hi gatir duḥkhaṁ dehavadbhir avāpyate</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/012_005.mp3",
+  },
+  {
+    Tittle: "Chapter-12 , Verse-8",
+    Text: "<p>mayy eva mana ādhatsva mayi buddhiṁ niveśhaya<br>nivasiṣhyasi mayy eva ata ūrdhvaṁ na sanśhayaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/012_008.mp3",
+  },
+  {
+    Tittle: "Chapter-12 , Verse-9",
+    Text: "<p>atha chittaṁ samādhātuṁ na śhaknoṣhi mayi sthiram<br>abhyāsa-yogena tato mām ichchhāptuṁ dhanañjaya</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/012_009.mp3",
+  },
+  {
+    Tittle: "Chapter-12 , Verse-10",
+    Text: "<p>abhyāse ’py asamartho ’si mat-karma-paramo bhava<br>mad-artham api karmāṇi kurvan siddhim avāpsyasi</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/012_010.mp3",
+  },
+  {
+    Tittle: "Chapter-12 , Verse-15",
+    Text: "<p>yasmān nodvijate loko lokān nodvijate cha yaḥ<br>harṣhāmarṣha-bhayodvegair mukto yaḥ sa cha me priyaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/012_015.mp3",
+  },
+  {
+    Tittle: "Chapter-12 , Verse-16",
+    Text: "<p>anapekṣhaḥ śhuchir dakṣha udāsīno gata-vyathaḥ<br>sarvārambha-parityāgī yo mad-bhaktaḥ sa me priyaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/012_016.mp3",
+  },
+  {
+    Tittle: "Chapter-12 , Verse-17",
+    Text: "<p>anapekṣhaḥ śhuchir dakṣha udāsīno gata-vyathaḥ<br>sarvārambha-parityāgī yo mad-bhaktaḥ sa me priyaḥ</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/012_017.mp3",
+  },
+  {
+    Tittle: "Chapter-18 , Verse-78",
+    Text: "<p>yatra yogeśhvaraḥ kṛiṣhṇo yatra pārtho dhanur-dharaḥ<br>tatra śhrīr vijayo bhūtir dhruvā nītir matir mama</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/018_078.mp3",
+  },
 ];
