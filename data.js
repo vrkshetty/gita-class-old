047e09c8224785376567f35a78c5ec738b558371
vrkshetty <vijayrkshetty@gmail.com>
2021-05-27 14:12:07 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,62 @@
+var myList = [
+  {
+    Tittle: "Chapter-1 , Verse-1",
+    Text: "dhṛitarāśhtra uvācha <br/>  <br/> dharma-kṣhetre kuru-kṣhetre samavetā yuyutsavaḥ<br/> māmakāḥ pāṇḍavāśhchaiva kimakurvata sañjaya</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/001_001.mp3",
+  },
+  {
+    Tittle: "Chapter-1 , Verse-2",
+    Text: "<p>sañjaya uvācha<br> <br/> dṛiṣhṭvā tu pāṇḍavānīkaṁ vyūḍhaṁ duryodhanastadā<br> āchāryamupasaṅgamya rājā vachanamabravīt</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/001_002.mp3",
+  },
+  {
+    Tittle: "Chapter-1 , Verse-3",
+    Text: "<p>paśhyaitāṁ pāṇḍu-putrāṇām āchārya mahatīṁ chamūm<br> vyūḍhāṁ drupada-putreṇa tava śhiṣhyeṇa dhīmatā</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/001_003.mp3",
+  },
+  {
+    Tittle: "Chapter-2 , Verse-7",
+    Text: "<p>kārpaṇya-doṣhopahata-svabhāvaḥ<br>pṛichchhāmi tvāṁ dharma-sammūḍha-chetāḥ<br>yach-chhreyaḥ syānniśhchitaṁ brūhi tanme<br>śhiṣhyaste ’haṁ śhādhi māṁ tvāṁ prapannam</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_007.mp3",
+  },
+  {
+    Tittle: "Chapter-2 , Verse-13",
+    Text: "<p>dehino ’smin yathā dehe kaumāraṁ yauvanaṁ jarā<br>tathā dehāntara-prāptir dhīras tatra na muhyati</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_013.mp3",
+  },
+  {
+    Tittle: "Chapter-2 , Verse-14",
+    Text: "<p>mātrā-sparśhās tu kaunteya śhītoṣhṇa-sukha-duḥkha-dāḥ<br>āgamāpāyino ’nityās tans-titikṣhasva bhārata</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_014.mp3",
+  },
+  {
+    Tittle: "Chapter-2 , Verse-20",
+    Text: "<p>na jāyate mriyate vā kadāchin<br>nāyaṁ bhūtvā bhavitā vā na bhūyaḥ<br>ajo nityaḥ śhāśhvato ’yaṁ purāṇo<br>na hanyate hanyamāne śharīre</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_020.mp3",
+  },
+  {
+    Tittle: "Chapter-2 , Verse-40",
+    Text: "<p>nehābhikrama-nāśho ’sti pratyavāyo na vidyate<br>svalpam apyasya dharmasya trāyate mahato bhayāt</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_040.mp3",
+  },
+  {
+    Tittle: "Chapter-2 , Verse-41",
+    Text: "<p>vyavasāyātmikā buddhir ekeha kuru-nandana<br>bahu-śhākhā hyanantāśh cha buddhayo ’vyavasāyinām</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_041.mp3",
+  },
+  {
+    Tittle: "Chapter-2 , Verse-59",
+    Text: "<p>viṣhayā vinivartante nirāhārasya dehinaḥ<br>rasa-varjaṁ raso ’pyasya paraṁ dṛiṣhṭvā nivartate</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_059.mp3",
+  },
+  {
+    Tittle: "Chapter-2 , Verse-62",
+    Text: "<p>dhyāyato viṣhayān puṁsaḥ saṅgas teṣhūpajāyate<br>saṅgāt sañjāyate kāmaḥ kāmāt krodho ’bhijāyate</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_062.mp3",
+  },
+  {
+    Tittle: "Chapter-2 , Verse-47",
+    Text: "<p>karmaṇy-evādhikāras te mā phaleṣhu kadāchana<br>mā karma-phala-hetur bhūr mā te saṅgo ’stvakarmaṇi</p>",
+    audoSrc: "https://www.holy-bhagavad-gita.org/public/audio/002_047.mp3",
+  },
+];
---
+++ fetch.js
@@ -0,0 +1,47 @@
+myListLen = myList.length;
+
+for (var i = 0; i < myListLen; i++) {
+  slokaTittle = myList[i].Tittle;
+  slokaParaText = myList[i].Text;
+  slokaAudoSrc = myList[i].audoSrc;
+  newDiv = document.createElement("div");
+  childDiv = document.createElement("div");
+  newDiv.id = "container";
+  newDiv.className = "chapter";
+  h2Text = document.createElement("h2");
+  h2Text.innerText = slokaTittle;
+  audio = document.createElement("AUDIO");
+  audio.setAttribute("controls", "src");
+  audio.setAttribute("src", slokaAudoSrc);
+  audio.setAttribute("controlsList", "nodownload");
+  audio.className = "audio";
+  newDiv.appendChild(h2Text);
+  childDiv.innerHTML = slokaParaText;
+  childDiv.className = "shlokaText";
+  h2Text.className = "chapterNumber";
+  newDiv.appendChild(childDiv);
+  newDiv.appendChild(audio);
+  document.body.appendChild(newDiv);
+}
+
+//original//
+// for (var i = 0; i < myListLen; i++) {
+//   slokaTittle = myList[i].Tittle;
+//   slokaParaText = myList[i].Text;
+//   slokaAudoSrc = myList[i].audoSrc;
+//   newDiv = document.createElement("div");
+//   childDiv = document.createElement("div");
+//   newDiv.id = "container";
+//   newDiv.className = "chapter";
+//   h2Text = document.createElement("h2");
+//   h2Text.innerText = slokaTittle;
+//   audio = document.createElement("AUDIO");
+//   audio.setAttribute("controls", "src");
+//   audio.setAttribute("src", slokaAudoSrc);
+//   audio.setAttribute("controlsList", "nodownload");
+//   newDiv.appendChild(h2Text);
+//   childDiv.innerHTML = slokaParaText;
+//   newDiv.appendChild(childDiv);
+//   newDiv.appendChild(audio);
+//   document.body.appendChild(newDiv);
+// }
